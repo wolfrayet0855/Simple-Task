7225c0a7597c81343ed85dca6c9af2d8d05c06c0
wolfayet0855 <online_alt02@yahoo.com>
2024-09-13 12:41:31 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by user on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
