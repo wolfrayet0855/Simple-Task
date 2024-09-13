b91b1c941bff42c7f8f4d13bef958b302c355581
wolfayet0855 <online_alt02@yahoo.com>
2024-09-13 14:56:08 -0400

    inital commit

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by user on 9/13/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
