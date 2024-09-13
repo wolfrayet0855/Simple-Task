2929c989606963839b3b15a74f6843fd874452b0
wolfayet0855 <online_alt02@yahoo.com>
2024-09-13 14:56:30 -0400

    inital commit

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct DetailView: View {
+    var passedValue: String // Don't initialize it -it will be passed from the parent view
+    
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "star")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            
+            Text("You are a super star!\n And you passed over the value \(passedValue)")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button("Click to return.") {
+                dismiss()
+            }
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(passedValue: "Item1")
 }
--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ToDoListView.swift
 //  ToDoList
 //
 //  Created by user on 9/13/24.
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    var toDos = ["Learn Swift",
+                 "Build Apps",
+                 "Change the World",
+                 "Bring the Aweseome",
+                 "Take a vacation"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(toDos, id: \.self) { toDo in
+                    NavigationLink {
+                        DetailView(passedValue: toDo)
+                    } label: {
+                        Text(toDo)
+                    }
+                }
+            }
+            .navigationTitle("To Do List")
+            .navigationBarTitleDisplayMode(.automatic)
+            .listStyle(.plain)
         }
-        .padding()
     }
 }
+      
 
 #Preview {
     ToDoListView()
